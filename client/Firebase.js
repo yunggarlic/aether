e09e5db8bb80e8abebb04b447516086a4516449c
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-01-13 11:06:38 -0600

    updates for heroku

--- client/Firebase.js
+++ client/Firebase.js
@@ -3,18 +3,14 @@ import 'firebase/auth'
 import 'firebase/firestore'
 // import firebaseConfig from '../config/secrets';
 
-
-const firebaseConfig = process.env
-  ? {
-      apiKey: process.env.apiKey,
-      authDomain: process.env.authDomain,
-      projectId: process.env.projectId,
-      storageBucket: process.env.storageBucket,
-      messagingSenderId: process.env.messagingSenderId,
-      appId: process.env.appId,
-      measurementId: process.env.measurementId,
-    }
-  : '../config/secrets'
+const firebaseConfig = {
+  apiKey: "AIzaSyAkdgarx-rprW-Iz-jQNCBExB_ovag2uOM",
+  authDomain: "aether-73e50.firebaseapp.com",
+  projectId: "aether-73e50",
+  storageBucket: "aether-73e50.appspot.com",
+  messagingSenderId: "364494291741",
+  appId: "1:364494291741:web:0fad00dd32d023260417f4"
+};
 
 // Initialize Firebase
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
